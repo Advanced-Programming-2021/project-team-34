045bb22dba73556849858279d8edc4255f21e4e5
lioneraV2002 <kamyar_maham1381@yahoo.com>
2021-04-25 00:17:32 +0430

    files added.

---
+++ LoginMenu.java
@@ -0,0 +1,36 @@
+﻿package Model 1;
+
+class LoginMenu
+{
+    
+    
+    
+    public final Matcher getCommandMatcher(String input, String regex)
+    {
+        
+    }    
+    
+    
+    public final void createUser(Matcher matcher)
+    {
+        
+    }    
+    
+    
+    public final void login(Matcher matcher)
+    {
+        
+    }    
+    
+    
+    public final void menuExit()
+    {
+        
+    }    
+    
+    
+    public final void showCurrentMenu()
+    {
+        
+    }    
+}
