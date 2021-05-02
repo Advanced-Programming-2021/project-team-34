8bf0f7332b71ceb3e3f5f4a685af1b016f870ea7
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-05-02 17:02:02 +0430

    code getCommandMatcher method in LoginMenu class

--- LoginMenu.java
+++ LoginMenu.java
@@ -1,4 +1,8 @@
-﻿package Model 1;
+﻿package Model
+
+import java.util.regex.*;
+
+1;
 
 class LoginMenu
 {
@@ -7,7 +11,9 @@ class LoginMenu
     
     public final Matcher getCommandMatcher(String input, String regex)
     {
-        
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher;
     }    
     
     
