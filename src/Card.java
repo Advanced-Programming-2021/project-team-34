c5ddd968cae8bad6defe8ece1a2da7a6c677900f
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-05-03 16:06:27 +0430

    Card setter and getters

--- src/Card.java
+++ src/Card.java
@@ -1,11 +1,41 @@
-﻿package Model 1;
+﻿
+public class Card {
+    private String name;
+    private String type;
+    private String description;
+    private Player owner;
+    private static ArrayList<Card> allCards;
 
-class Card
-{
-    private String name;		
-    private String type;		
-    private String description;		
-    private Player owner;		
-    private static ArrayList<Card> allCards;		
-    
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Player getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
 }
--- src/LoginMenu.java
+++ src/LoginMenu.java
@@ -1,36 +1,26 @@
-﻿package Model 1;
+﻿import java.util.regex.*;
 
-class LoginMenu
-{
-    
-    
-    
-    public final Matcher getCommandMatcher(String input, String regex)
-    {
-        
-    }    
-    
-    
-    public final void createUser(Matcher matcher)
-    {
-        
-    }    
-    
-    
-    public final void login(Matcher matcher)
-    {
-        
-    }    
-    
-    
-    public final void menuExit()
-    {
-        
-    }    
-    
-    
-    public final void showCurrentMenu()
-    {
-        
-    }    
+public class LoginMenu {
+
+    public Matcher getCommandMatcher(String input, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher;
+    }
+
+    public final void createUser(Matcher matcher) {
+        // \\s*user\\s+creat\\s*username
+    }
+
+    public final void login(Matcher matcher) {
+
+    }
+
+    public final void menuExit() {
+
+    }
+
+    public final void showCurrentMenu() {
+
+    }
 }
