1f2910052259b660c144a12679c31b4fd24ea2c1
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-05-04 11:55:16 +0430

    به‌روز شد Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -5,6 +5,43 @@ public class Deck {
     private ArrayList<Card> mainDeck;
     private ArrayList<Card> sideDeck;
     private Player owner;
+
+    public Deck(String name , Player owner) {
+        this.name = name;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Card> getMainDeck() {
+        return this.mainDeck;
+    }
+
+    public void setMainDeck(ArrayList<Card> mainDeck) {
+        this.mainDeck = mainDeck;
+    }
+
+    public ArrayList<Card> getSideDeck() {
+        return this.sideDeck;
+    }
+
+    public void setSideDeck(ArrayList<Card> sideDeck) {
+        this.sideDeck = sideDeck;
+    }
+
+    public Player getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
     
     public void addCardToMainDeck(Card card) {
         mainDeck.add(card);
