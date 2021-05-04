4d7f905a56ba274dea4c75be87cdc15f065f5532
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-05-04 14:40:49 +0430

    افزوده‌شد امّا تنها متغیّرهایش و نه توابعش Monster کلاس

---
+++ src/Monster.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class Monster extends Card {
+    private int attPower;//attack power
+    private int defPower;//defence power
+    private int level;
+    private String state;
+    private int defAttPower;//default attack power
+    private int defDefPower;//default defence power
+    public String Ritual;
+    private ArrayList<Monster> monsters;
+    private String monsterName;
+    private int timesAffected;
+    //TO DO : 
+    /* To Add Constructor */
+    //!!!!!!!!!!!!!!!!!!!!!!
+    //TO DO : 
+    /* To Add All Methods */
+}
