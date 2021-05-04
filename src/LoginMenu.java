f1fdd5fdc85b85b27988971117273437554a9bda
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-05-04 17:15:30 +0430

    افزوده‌شد LoginMenu.java

---
+++ src/LoginMenu.java
@@ -0,0 +1,20 @@
+
+public class LoginMenu {
+    public Matcher getCommandMatcher(String input, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(input);
+    }
+    public createUser(Matcher matcher){
+        ;
+    }
+    public void login(Matcher matcher) {
+        ;
+    }
+    public menuExit(){
+        ;
+    }
+    public showCurrentMenu() {
+        ;
+    }
+
+}
