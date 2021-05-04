ae0d849ff6f1b95458d3ca266f05696cec3125ca
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-05-04 15:19:12 +0430

    ها افزوده‌شدند getter ها و setter Monster.java به

--- src/Monster.java
+++ src/Monster.java
@@ -9,10 +9,83 @@ public class Monster extends Card {
     public String Ritual;
     private ArrayList<Monster> monsters;
     private String monsterName;
-    private int timesAffected;
+    private int timesAffected=0;
     //TO DO : 
     /* To Add Constructor */
     //!!!!!!!!!!!!!!!!!!!!!!
     //TO DO : 
     /* To Add All Methods */
+
+    public int getAttPower() {
+        return this.attPower;
+    }
+
+    public void setAttPower(int attPower) {
+        this.attPower = attPower;
+    }
+
+    public int getDefPower() {
+        return this.defPower;
+    }
+
+    public void setDefPower(int defPower) {
+        this.defPower = defPower;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getDefAttPower() {
+        return this.defAttPower;
+    }
+
+    public void setDefAttPower(int defAttPower) {
+        this.defAttPower = defAttPower;
+    }
+
+    public int getDefDefPower() {
+        return this.defDefPower;
+    }
+
+    public void setDefDefPower(int defDefPower) {
+        this.defDefPower = defDefPower;
+    }
+
+    public String getRitual() {
+        return this.Ritual;
+    }
+
+    public void setRitual(String Ritual) {
+        this.Ritual = Ritual;
+    }
+
+    public String getMonsterName() {
+        return this.monsterName;
+    }
+
+    public void setMonsterName(String monsterName) {
+        this.monsterName = monsterName;
+    }
+
+    public int getTimesAffected() {
+        return this.timesAffected;
+    }
+
+    public void increaseTimesAffected() {
+        this.timesAffected++;
+    }
+    
 }
