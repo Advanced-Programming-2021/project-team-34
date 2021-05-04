1cd52c9cc1665b35deee7e13323c21a9aef05199
AliAgha888 <arahmizad@gmail.com>
2021-05-04 22:17:41 +0430

    Login function of Controller class completed

---
+++ Board.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Board {
+	private char[][] player1Board;
+	private char[][] player2Board;
+	private ArrayList<Card> player1Graveyard;
+	private ArrayList<Card> player2Graveyard;
+	private ArrayList<Card> player1Hand;
+	private ArrayList<Card> player2Hand;
+	private ArrayList<Card> player1Monsters;
+	private ArrayList<Card> player2Monsters;
+	private ArrayList<Card> player1TrapsAndSpells;
+	private ArrayList<Card> player2TrapsAndSpells;
+	
+	public void showPlayer1Board() {
+	
+	}
+	
+	public void showPlayer2Board() {
+		
+	}
+	
+	public void getPlayer1Board() {
+	
+	}
+	
+	public void getPlayer2Board() {
+		
+	}
+	
+	public void getPlayer1Graveyard() {
+	
+	}
+	
+	public void getPlayer2Graveyard() {
+		
+	}
+	
+	public void getPlayer1Hand() {
+	
+	}
+	
+	public void getPlayer2Hand() {
+		
+	}
+	
+	public void getPlayer1Monsters() {
+	
+	}
+	
+	public void getPlayer2Monsters() {
+		
+	}
+	
+	public void getPlayer1TrapsAndSpells() {
+	
+	}
+	
+	public void getPlayer2TrapsAndSpells() {
+		
+	}
+}
--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,6 @@
 import java.util.regex.*;
 import java.util.Scanner;
+
 public class Controller {
     public static Matcher matcher;
     public static Matcher matcher1;
@@ -8,26 +9,60 @@ public class Controller {
     public static Matcher matcher4;
     public static Matcher matcher5;
     public static Matcher matcher6;
-    Scanner scanner = new Scanner(System.in);
-    String input;
+    public static Scanner scanner = new Scanner(System.in);
+    private static String input;
+
     public static void runLoginMenu() {
         LoginMenu menu = new LoginMenu();
-        while(ture){
-            input = scanner.nextLine();
-            matcher = getMatch(input,"\\s*user\\s+create\\s+username\\s+(\\w+)\\s+password\\s+\\w+\\s+nickname\\s+\\w+\\s*");
-            mathcer1=getMatch(input,"\\s*user\\s+create\\s+username\\s+(\\w+)\\s+password\\s+\\w+\\s+nickname\\s+\\w+\\s*");
-            mathcer2=getMatch(input,"\\s*user\\s+create\\s+username\\s+(\\w+)\\s+password\\s+\\w+\\s+nickname\\s+\\w+\\s*");
-            matcher3=getMatch(input,"\\s*user\\s+create\\s+username\\s+(\\w+)\\s+password\\s+\\w+\\s+nickname\\s+\\w+\\s*");
-            matcher4=getMatch(i)
-            if(mathcer.matches()&&matcher1.find()&&matcher2.find()&&mathcer3.find()){
-
+        while (true) {
+            input = scanner.nextLine().trim();
+            matcher = getMatch(input, "^\\s*user\\s+create\\s+(--username|-u)\\s+(\\w+)\\s+(--password|-p)\\s+(\\w+)\\s+(--nickname|-n)\\s+(\\w+)\\s*$");
+            if (matcher.find()) {
+                String username = matcher.group(1);
+                String password = matcher.group(2);
+                String nickname = matcher.group(3);
+                new Player(username, password, nickname);
+            }
+            matcher = getMatch(input, "^\\s*user\\s+create\\s+(--username|-u)\\s+(\\w+)\\s+(--nickname|-n)\\s+(\\w+)\\s+(--password|-p)\\s+(\\w+)\\s*$");
+            if (matcher.find()) {
+                String username = matcher.group(1);
+                String password = matcher.group(3);
+                String nickname = matcher.group(2);
+                new Player(username, password, nickname);
+            }
+            matcher = getMatch(input, "^\\s*user\\s+create\\s+(--nickname|-n)\\s+(\\w+)\\s+(--username|-u)\\s+(\\w+)\\s+(--password|-p)\\s+(\\w+)\\s*$");
+            if (matcher.find()) {
+                String username = matcher.group(2);
+                String password = matcher.group(3);
+                String nickname = matcher.group(1);
+                new Player(username, password, nickname);
+            }
+            matcher = getMatch(input, "^\\s*user\\s+create\\s+(--nickname|-n)\\s+(\\w+)\\s+(--password|-p)\\s+(\\w+)\\s+(--username|-u)\\s+(\\w+)\\s*$");
+            if (matcher.find()) {
+                String username = matcher.group(3);
+                String password = matcher.group(2);
+                String nickname = matcher.group(1);
+                new Player(username, password, nickname);
+            }
+            matcher = getMatch(input, "^\\s*user\\s+create\\s+(--password|-p)\\s+(\\w+)\\s+(--username|-u)\\s+(\\w+)\\s+(--nickname|-n)\\s+(\\w+)\\s*$");
+            if (matcher.find()) {
+                String username = matcher.group(2);
+                String password = matcher.group(1);
+                String nickname = matcher.group(3);
+                new Player(username, password, nickname);
+            }
+            matcher = getMatch(input, "^\\s*user\\s+create\\s+(--password|-p)\\s+(\\w+)\\s+(--nickname|-n)\\s+(\\w+)\\s+(--username|-u)\\s+(\\w+)\\s*$");
+            if (matcher.find()) {
+                String username = matcher.group(3);
+                String password = matcher.group(1);
+                String nickname = matcher.group(2);
+                new Player(username, password, nickname);
             }
         }
     }
 
 
-
-    public Matcher getMatch(String input, String regex) {
+    public static Matcher getMatch(String input, String regex) {
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(input);
     }
