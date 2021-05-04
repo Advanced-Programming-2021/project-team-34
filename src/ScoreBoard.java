bfa51f2715810592971f6fe1cd896324f61c768a
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-05-04 14:29:00 +0430

    افزوده‌شد ScoreBoard.java

---
+++ src/ScoreBoard.java
@@ -0,0 +1,15 @@
+//import
+public class ScoreBoard {
+    public void showScoreboard() {
+        ;
+    }  
+    public void enterMenu() {
+        ;
+    }
+    public void exitMenu() {
+        ;
+    }
+    public void showCurrentMenu() {
+        
+    }
+}
