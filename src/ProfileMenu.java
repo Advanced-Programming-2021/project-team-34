33407c0e0e23ec6ace500f2d7d5ef5be4db7f8d3
lioneraV2002 <kamyar_maham1381@yahoo.com>
2021-05-10 16:43:50 +0430

    Profile Menu almost complete

--- src/ProfileMenu.java
+++ src/ProfileMenu.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class ProfileMenu {
+import java.util.Scanner;
+
+public class ProfileMenu {
+    private static Player currentUser;
+    private static Scanner scanner;
+
+    public static void runProfileMenu(Player newUser) {
+
+        currentUser = newUser;
+        String userCommand = scanner.nextLine().trim();
+
+        while (true) {
+            if (userCommand.startsWith("Menu enter"))
+        }
+
+    }
 }
