eb8fed1189facdb7794f52f888e0d3176ec52451
lioneraV2002 <kamyar_maham1381@yahoo.com>
2021-05-12 02:36:27 +0430

    starting the spell class

--- src/SpellAndTrap.java
+++ src/SpellAndTrap.java
@@ -1,2 +1,12 @@
-public class SpellAndTrap {
+public class SpellAndTrap extends Card{
+
+    private String icon;
+    public SpellAndTrap(int price, String name, String type, String description, String icon) {
+        super(price, name, type, description);
+        this.icon = icon;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
 }
