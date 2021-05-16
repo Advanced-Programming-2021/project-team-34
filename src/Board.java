fff642d992b81300ff1d5e057551ee27f7fed7f6
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-05-16 23:30:32 +0430

    .افزوده‌شد Board.java کشیدن صفحه و توابع و مقادیر مورد نیاز دیگری به

--- src/Board.java
+++ src/Board.java
@@ -11,55 +11,216 @@ public class Board {
     private ArrayList<Card> player2Monsters;
     private ArrayList<Card> player1TrapsAndSpells;
     private ArrayList<Card> player2TrapsAndSpells;
+    private ArrayList<Card> player1RemainedDeck, player2RemainedDeck;
+    private Card fz1 , fz2;
+    private Player you, opponent, player1, player2;
 
-
-
-
-    public void showPlayer1Board() {
-
+    public Board(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
-    public void showPlayer2Board() {
-
+    public void show(Player you){
+        this.you = you;
+        if (this.player1.equals(you)||player1==you){
+            opponent = player2;
+        }else {
+            opponent = player1;
+        }
+        showOpponentBoard();
+        System.out.println("\n\n--------------------------\n");
+        showYourBoard();
     }
-
-    public void getPlayer1Board() {
-
+    private void showOpponentBoard(){
+        showNickname(opponent);
+        goToNextColumn();
+        showLifePoint(opponent);
+        goToNextRow();
+        goToNextColumn();
+        showInHandCards(opponent);
+        goToNextRow();
+        showDN(opponent);
+        goToNextRow();
+        showSpellsAndTraps(opponent);
+        goToNextRow();
+        showMonsters(opponent);
+        showGY(opponent);
+        goToNextColumn();goToNextColumn();goToNextColumn();goToNextColumn();goToNextColumn();goToNextColumn();
+        showFZ(opponent);
     }
-
-    public void getPlayer2Board() {
-
+    private  void  showYourBoard(){
+        showFZ(you);
+        goToNextColumn();goToNextColumn();goToNextColumn();goToNextColumn();goToNextColumn();goToNextColumn();
+        showGY(you);
+        showMonsters(you);
+        goToNextRow();
+        showSpellsAndTraps(you);
+        goToNextRow();
+        showDN(you);
+        goToNextRow();
+        showInHandCards(you);
+        goToNextRow();
+        showLifePoint(you);
+        goToNextColumn();
+        showNickname(you);
     }
-
-    public void getPlayer1Graveyard() {
-
+    private void showNickname(Player player){
+        write(player.getNickname());
     }
-
-    public void getPlayer2Graveyard() {
-
+    private void showLifePoint(Player  player){
+        write(""+(player.lifePoint));
     }
-
-    public void getPlayer1Hand() {
-
+    private void showInHandCards(Player player){
+        if(player.equals(player1)||player==player1){
+            for (Card c :
+                    player1Hand) {
+                System.out.println("c");
+                goToNextColumn();
+            }
+        }else {
+            for (Card c :
+                    player2Hand) {
+                System.out.println("c");
+                goToNextColumn();
+            }
+        }
     }
-
-    public void getPlayer2Hand() {
-
+    private void showDN(Player player){
+        if (player.equals(player1)||player==player1 ){
+            write(""+player1RemainedDeck.size());
+        }else {
+            write(""+player2RemainedDeck.size());
+        }
     }
-
-    public void getPlayer1Monsters() {
-
+    private void showGY(Player player){
+        if(player.equals(player1)||player==player1){
+            write(""+player1Graveyard.size());
+        }else{
+            write(""+player2Graveyard.size());
+        }
     }
-
-    public void getPlayer2Monsters() {
-
+    private void showSpellsAndTraps(Player player){
+        if((player.equals(player1)||player==player1)&&(player.equals(you))||player==you){
+            write(""+player1Board[0][4]);goToNextColumn();
+            write(""+player1Board[0][2]);goToNextColumn();
+            write(""+player1Board[0][1]);goToNextColumn();
+            write(""+player1Board[0][3]);goToNextColumn();
+            write(""+player1Board[0][5]);
+        }else if((player.equals(player2)||player==player2)&&(player.equals(you))||player==you){
+            write(""+player2Board[0][4]);goToNextColumn();
+            write(""+player2Board[0][2]);goToNextColumn();
+            write(""+player2Board[0][1]);goToNextColumn();
+            write(""+player2Board[0][3]);goToNextColumn();
+            write(""+player2Board[0][5]);
+        }else if((player.equals(player2)||player==player2)&&(player.equals(opponent))||player==opponent){
+            write(""+player2Board[0][5]);goToNextColumn();
+            write(""+player2Board[0][3]);goToNextColumn();
+            write(""+player2Board[0][1]);goToNextColumn();
+            write(""+player2Board[0][2]);goToNextColumn();
+            write(""+player2Board[0][4]);
+        }else{
+            write(""+player1Board[0][5]);goToNextColumn();
+            write(""+player1Board[0][3]);goToNextColumn();
+            write(""+player1Board[0][1]);goToNextColumn();
+            write(""+player1Board[0][2]);goToNextColumn();
+            write(""+player1Board[0][4]);
+        }
     }
-
-    public void getPlayer1TrapsAndSpells() {
-
+    private void showMonsters(Player player){
+        if((player.equals(player1)||player==player1)&&(player.equals(you))||player==you){
+            write(""+player1Board[1][4]);goToNextColumn();
+            write(""+player1Board[1][2]);goToNextColumn();
+            write(""+player1Board[1][1]);goToNextColumn();
+            write(""+player1Board[1][3]);goToNextColumn();
+            write(""+player1Board[1][5]);
+        }else if((player.equals(player2)||player==player2)&&(player.equals(you))||player==you){
+            write(""+player2Board[1][4]);goToNextColumn();
+            write(""+player2Board[1][2]);goToNextColumn();
+            write(""+player2Board[1][1]);goToNextColumn();
+            write(""+player2Board[1][3]);goToNextColumn();
+            write(""+player2Board[1][5]);
+        }else if((player.equals(player2)||player==player2)&&(player.equals(opponent))||player==opponent){
+            write(""+player2Board[1][5]);goToNextColumn();
+            write(""+player2Board[1][3]);goToNextColumn();
+            write(""+player2Board[1][1]);goToNextColumn();
+            write(""+player2Board[1][2]);goToNextColumn();
+            write(""+player2Board[1][4]);
+        }else{
+            write(""+player1Board[1][5]);goToNextColumn();
+            write(""+player1Board[1][3]);goToNextColumn();
+            write(""+player1Board[1][1]);goToNextColumn();
+            write(""+player1Board[1][2]);goToNextColumn();
+            write(""+player1Board[1][4]);
+        }
     }
-
-    public void getPlayer2TrapsAndSpells() {
-
+    private void showFZ(Player player){
+        if (player.equals(player1)||player==player1){
+            if (fz1 == null) {
+                write("E");
+            }else {write ("O");}
+        }else{
+            if (fz2 == null) {
+                write(("E"));
+            }else {write("O");}
+        }
     }
+//    public void showPlayer1Board() {
+//
+//    }
+//
+//    public void showPlayer2Board() {
+//
+//    }
+//
+//    public void getPlayer1Board() {
+//
+//    }
+//
+//    public void getPlayer2Board() {
+//
+//    }
+//
+//    public void getPlayer1Graveyard() {
+//
+//    }
+//
+//    public void getPlayer2Graveyard() {
+//
+//    }
+//
+//    public void getPlayer1Hand() {
+//
+//    }
+//
+//    public void getPlayer2Hand() {
+//
+//    }
+//
+//    public void getPlayer1Monsters() {
+//
+//    }
+//
+//    public void getPlayer2Monsters() {
+//
+//    }
+//
+//    public void getPlayer1TrapsAndSpells() {
+//
+//    }
+//
+//    public void getPlayer2TrapsAndSpells() {
+//
+//    }
+    public static void goToNextRow(){
+        System.out.println("");
+    }
+    public static void goToNextColumn(){
+        System.out.println("\t");
+    }
+    public static void write(String string){
+        System.out.println(string);
+    }
+//    public static void write(char character){
+//        System.out.println(character);
+//    }
 }
