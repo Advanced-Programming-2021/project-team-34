3208ae6962283fc90cb83c412ae0af59bdad3bf6
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-06-16 10:23:24 +0430

    .افزوده‌شد view اش ، در initialize و ShopMenu.java

---
+++ YOGIOH/src/main/java/View/Menus/ShopMenu.java
@@ -0,0 +1,64 @@
+package View.Menus;
+
+import Controller.MenuController;
+import Controller.MenuNames;
+import View.CommandHelper.Command;
+import View.CommandHelper.CommandType;
+
+public class ShopMenu extends ViewMenu {
+    static boolean toContinue = true;
+    public static void run() {
+        initializeMenu();
+        doMain();
+    }
+
+    private static void doMain() {
+        String input;
+        Command myCommand;
+        String typeOfMyCommand;
+        while (toContinue) {
+            show("SHOP MENU");
+            input = input();
+            myCommand = new Command(input);
+            typeOfMyCommand = myCommand.getType();
+            switch (typeOfMyCommand) {
+                case "invalid type" :
+                    print("The command you inputted is not valid in this menu!!!");
+                    break;
+                case "exit":
+                    MenuController.setMenuName(MenuNames.Exit);
+                    toContinue = false;
+                    break;
+                case "show current menu":
+                    print("Shop Menu");
+                    break;
+            }
+            getConfirmation();
+        }
+    }
+
+    private static void initializeMenu() {
+        toContinue = true;
+        Command.clearValidCommandTypes();
+        CommandType commandType = new CommandType();
+        initializeBuyCardCommandType();
+        initializeShowAllCommandType();
+    }
+
+    private static void initializeShowAllCommandType() {
+        CommandType commandType = new CommandType();
+        commandType.setName("show all");
+        commandType.setMainPart("shop show all");
+        Command.addCommandType(commandType);
+    }
+
+    private static void initializeBuyCardCommandType() {
+        CommandType commandType = new CommandType();
+        commandType.setName("buy card");
+        commandType.setMainPart("shop");
+        commandType.getField("buy");
+        Command.addCommandType(commandType);
+    }
+
+
+}
