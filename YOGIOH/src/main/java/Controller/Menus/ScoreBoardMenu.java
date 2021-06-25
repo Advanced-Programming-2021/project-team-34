b87631156ec601e9b58018c892de964a0da65ab7
lioneraV2002 <kamyar_maham1381@yahoo.com>
2021-06-26 00:49:34 +0430

    show method added to score board until upcoming changes

--- YOGIOH/src/main/java/Controller/Menus/ScoreBoardMenu.java
+++ YOGIOH/src/main/java/Controller/Menus/ScoreBoardMenu.java
@@ -1,2 +1,13 @@
-package Controller.Menus;public class ScoreBoardMenu {
+package Controller.Menus;
+
+import Model.User;
+
+public class ScoreBoardMenu {
+    public String showRankings(){
+        StringBuilder scoreBoard = new StringBuilder();
+        for(User user : User.users) {
+            scoreBoard.append(user.getRank()).append("_  ").append(user.getNickname()).append(":  ").append(user.getHighScore());
+        }
+        return scoreBoard.toString();
+    }
 }
