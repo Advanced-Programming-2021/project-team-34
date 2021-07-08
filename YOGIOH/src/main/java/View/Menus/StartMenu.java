cddbf03556198470ab9c077c25b7d1706766e4d0
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-07-08 14:22:25 +0430

    زده‌شد StartMenu گرافیک

--- YOGIOH/src/main/java/View/Menus/StartMenu.java
+++ YOGIOH/src/main/java/View/Menus/StartMenu.java
@@ -4,108 +4,225 @@ import Controller.MenuController;
 import Controller.MenuNames;
 import View.CommandHelper.Command;
 import View.CommandHelper.CommandType;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class StartMenu extends ViewMenu {
     static boolean toContinue = true;
+    static Scene scene;
+    static StartMenu startMenu = new StartMenu();
+    public GridPane usernameToLoginGridPane, passwordToLoginGridPane, stayInGridPane, resultLoginGridPane,
+             usernameToRegisterGridPane, passwordToRegisterGridPane, nicknameToRegisterGridPane,
+             resultOfRegisteringGridPane;
     public static void run() {
-        initializeMenu();
-        doMain();
-    }
-
-    protected static void doMain() {
-        String input;
-        Command myCommand;
-        String typeOfMyCommand;
-        while (toContinue) {
-            show("LOGIN MENU");
-            input = input();
-            myCommand = new Command(input);
-            typeOfMyCommand = myCommand.getType();
-            switch (typeOfMyCommand) {
-                case "invalid type" :
-                    print("The command you inputted is not valid in this menu!!!");
-                    break;
-                case "login":
-                    login(myCommand);
-                    break;
-                case "exit":
-                    MenuController.setMenuName(MenuNames.Exit);
-                    toContinue = false;
-                    break;
-                case "show current menu":
-                    print("Login Menu");
-                    break;
-                case "create user":
-                    createUser(myCommand);
-                    break;
+        try {
+            launch();
+        } catch (Exception e) {
+            try {
+                startMenu.start(stage);
+            } catch (Exception exception) {
+                MenuController.setMenuName(MenuNames.Exit);
             }
-            getConfirmation();
         }
     }
 
-    private static void login(Command myCommand) {
-        boolean success = Controller.Menus.StartMenu.login(myCommand.getField("username"),
-                myCommand.getField("password"));
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane pane = FXMLLoader.load(getClass().getResource("StartMenu.fxml"));
+        scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("YU_GI_OH!");
+        primaryStage.show();
+    }
+
+
+
+    public void registerByMouse(MouseEvent mouseEvent) {
+        if (isPrimary(mouseEvent)) {
+            register();
+        }
+    }
+
+    private void register() {
+        String username = getStringOfAGridPane(usernameToRegisterGridPane);
+        String password = getStringOfAGridPane(passwordToRegisterGridPane);
+        String nickname = getStringOfAGridPane(nicknameToRegisterGridPane);
+        boolean success = Controller.Menus.StartMenu.register(username, nickname, password);
         if (success) {
-            print("You logged in successfully");
-            toContinue = false;
-            // in controller : MenuController.setMenuName(MenuNames.MainMenu);
+            message("حساب شما پیروزمندانه ساخته‌شد" , resultOfRegisteringGridPane);
         } else {
-            print(Controller.Menus.StartMenu.getError());
+            message(Controller.Menus.StartMenu.getError() , resultOfRegisteringGridPane);
         }
     }
 
-    private static void createUser(Command myCommand) {
-        boolean success = Controller.Menus.StartMenu.register(myCommand.getField("username"),
-                myCommand.getField("nickname"),
-                myCommand.getField("password"));
+    public void registerByKey(KeyEvent keyEvent) {
+        if (isChoiceKey(keyEvent)) {
+            register();
+        }
+    }
+
+    public void loginByMouse(MouseEvent mouseEvent) {
+        if (isPrimary(mouseEvent)) {
+            login();
+        }
+    }
+
+    private void login() {
+        String username = getStringOfAGridPane(usernameToLoginGridPane);
+        String password = getStringOfAGridPane(passwordToLoginGridPane);
+        boolean success = Controller.Menus.StartMenu.login(username, password);
         if (success) {
-            print("user created successfully");
+            try {
+                this.stop();
+                MainMenu.run();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else {
-            print(Controller.Menus.StartMenu.getError());
+            message(Controller.Menus.StartMenu.getError() , resultLoginGridPane);
         }
     }
 
-    private static void initializeMenu() {
-        toContinue = true;
-        Command.clearValidCommandTypes();
-        initializeLoginCommandType();
-        initializeExitCommandType();
-        initializeShowCurrentMenuCommandType();
-        initializeCreateUserCommandType();
+    public void loginByKey(KeyEvent keyEvent) {
+        if (isChoiceKey(keyEvent)) {
+            login();
+        }
     }
 
-    private static void initializeCreateUserCommandType() {
-        CommandType commandType = new CommandType();
-        commandType.setName("create user");
-        commandType.setMainPart("user create");
-        commandType.addField("username");
-        commandType.addField("password");
-        commandType.addField("nickname");
-        Command.addCommandType(commandType);
-    }
 
-    private static void initializeShowCurrentMenuCommandType() {
-        CommandType commandType = new CommandType();
-        commandType.setMainPart("menu show-current");
-        commandType.setName("show current menu");
-        Command.addCommandType(commandType);
-    }
+//    public static void run1() {
+//        initializeMenu();
+//        doMain();
+//    }
+//
+//    protected static void doMain() {
+//        String input;
+//        Command myCommand;
+//        String typeOfMyCommand;
+//        while (toContinue) {
+//            show("LOGIN MENU");
+//            input = input();
+//            myCommand = new Command(input);
+//            typeOfMyCommand = myCommand.getType();
+//            switch (typeOfMyCommand) {
+//                case "invalid type" :
+//                    print("The command you inputted is not valid in this menu!!!");
+//                    break;
+//                case "login":
+//                    login(myCommand);
+//                    break;
+//                case "exit":
+//                    MenuController.setMenuName(MenuNames.Exit);
+//                    toContinue = false;
+//                    break;
+//                case "show current menu":
+//                    print("Login Menu");
+//                    break;
+//                case "create user":
+//                    createUser(myCommand);
+//                    break;
+//            }
+//            getConfirmation();
+//        }
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//    // for phase 1
+//    private static void login(Command myCommand) {
+//        boolean success = Controller.Menus.StartMenu.login(myCommand.getField("username"),
+//                myCommand.getField("password"));
+//        if (success) {
+//            print("You logged in successfully");
+//            toContinue = false;
+//            // in controller : MenuController.setMenuName(MenuNames.MainMenu);
+//        } else {
+//            print(Controller.Menus.StartMenu.getError());
+//        }
+//    }
+//
+//    private static void createUser(Command myCommand) {
+//        boolean success = Controller.Menus.StartMenu.register(myCommand.getField("username"),
+//                myCommand.getField("nickname"),
+//                myCommand.getField("password"));
+//        if (success) {
+//            print("user created successfully");
+//        } else {
+//            print(Controller.Menus.StartMenu.getError());
+//        }
+//    }
+//
+//    private static void initializeMenu() {
+//        toContinue = true;
+//        Command.clearValidCommandTypes();
+//        initializeLoginCommandType();
+//        initializeExitCommandType();
+//        initializeShowCurrentMenuCommandType();
+//        initializeCreateUserCommandType();
+//    }
+//
+//    private static void initializeCreateUserCommandType() {
+//        CommandType commandType = new CommandType();
+//        commandType.setName("create user");
+//        commandType.setMainPart("user create");
+//        commandType.addField("username");
+//        commandType.addField("password");
+//        commandType.addField("nickname");
+//        Command.addCommandType(commandType);
+//    }
+//
+//    private static void initializeShowCurrentMenuCommandType() {
+//        CommandType commandType = new CommandType();
+//        commandType.setMainPart("menu show-current");
+//        commandType.setName("show current menu");
+//        Command.addCommandType(commandType);
+//    }
+//
+//    private static void initializeExitCommandType() {
+//        CommandType commandType = new CommandType();
+//        commandType.setName("exit");
+//        commandType.setMainPart("menu exit");
+//        Command.addCommandType(commandType);
+//    }
+//
+//    private static void initializeLoginCommandType() {
+//        CommandType commandType = new CommandType();
+//        commandType.setMainPart("user login");
+//        commandType.setName("login");
+//        commandType.addField("username");
+//        commandType.addField("password");
+//        Command.addCommandType(commandType);
+//    }
 
-    private static void initializeExitCommandType() {
-        CommandType commandType = new CommandType();
-        commandType.setName("exit");
-        commandType.setMainPart("menu exit");
-        Command.addCommandType(commandType);
-    }
-
-    private static void initializeLoginCommandType() {
-        CommandType commandType = new CommandType();
-        commandType.setMainPart("user login");
-        commandType.setName("login");
-        commandType.addField("username");
-        commandType.addField("password");
-        Command.addCommandType(commandType);
-    }
 
 }
--- YOGIOH/src/main/java/View/Menus/ViewMenu.java
+++ YOGIOH/src/main/java/View/Menus/ViewMenu.java
@@ -1,9 +1,19 @@
 package View.Menus;
 
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 import java.util.Scanner;
 
-public class ViewMenu {
+public class ViewMenu extends Application {
     static Scanner scanner = new Scanner(System.in);
+    public static Stage stage;// = new Stage();
     final static int NUMBEROFLINESTOSKIP = 100;
     public static void print(String stringToBeWritten) {
         System.out.println(stringToBeWritten);
@@ -34,6 +44,79 @@ public class ViewMenu {
         manyLinesAfter();
         showTitle(menuName);
         manyLinesAfter(5);
+    }
+
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+    public boolean isPrimary(MouseEvent mouseEvent) {
+        String mouseButton = mouseEvent.getButton().toString();
+        if (mouseButton.equals("PRIMARY")) {
+            return true;
+        }
+        return false;
+
+    }
+    public boolean isSecondary(MouseEvent mouseEvent) {
+        String mouseButton = mouseEvent.getButton().toString();
+        if (mouseButton.equals("SECONDARY")) {
+            return true;
+        }return false;
+    }
+    public boolean isChoiceKey(KeyEvent keyEvent) {
+        String keyName = keyEvent.getCode().toString();
+        if (keyName.equals("ENTER") || keyName.equals("SPACE")) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * This method gets string of the first child of a gridPane if that child is a TextField or a PasswordField
+     * @param gridPane the gridPane you want to get text from.
+     *
+     * @param isPassword is a boolean which should be true if the firs child is a PasswordChild and false if it is a TextField
+     *                   and you can don't give it so it is false be default (it will be considered as aTextField).
+     */
+    public String getStringOfAGridPane(GridPane gridPane , boolean isPassword) {
+        Object[] children = (Object[]) gridPane.getChildren().toArray();
+        return (isPassword ? (((PasswordField) children[0]).getText()) : ((TextField) children[0]).getText());
+    }
+    /**
+     * This method is actually getStringOfAGridPane(GridPane gridPane , boolean isPassword) which isPassword is false
+     *
+     */
+    public String getStringOfAGridPane(GridPane gridPane) {
+        return getStringOfAGridPane(gridPane , false);
+    }
+
+    /**
+     * This method writes a message on a place of our scene where is a Label of javafx which is the first child
+     *          of a gridPane.
+     * @param message : String the message you want to show to user.
+     * @param gridPane : GridPane the gridPane which we want to set text of that first child which is a Label.
+     */
+    public void message(String message , GridPane gridPane){
+        Object[] children = gridPane.getChildren().toArray();
+        Label label = (Label) children[0];
+        label.setText(message);
+        System.out.println(message);
     }
 
 }
