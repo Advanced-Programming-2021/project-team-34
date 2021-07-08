34659aa0993c3d72eafa15c76d50ec80d031a245
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-07-08 23:35:18 +0430

    پیشرفت DuelMenu

--- YOGIOH/src/main/java/View/Menus/DuelMenu.java
+++ YOGIOH/src/main/java/View/Menus/DuelMenu.java
@@ -4,12 +4,17 @@ import Controller.MenuController;
 import Controller.MenuNames;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class DuelMenu extends ViewMenu{
     static DuelMenu duelMenu = new DuelMenu();
     Scene scene;
+    public GridPane usernameOfOpponentGridPane, roundsGridPane, resultGridPane;
+    int round = 1;
     public static void run() {
         try {
             duelMenu.start(stage);
@@ -26,5 +31,50 @@ public class DuelMenu extends ViewMenu{
         primaryStage.setTitle("YU_GI_OH!");
         stage = primaryStage;
         stage.show();
+    }
+
+    public void backByMouse(MouseEvent mouseEvent) {
+        if (isPrimary(mouseEvent))
+            MainMenu.run();
+    }
+
+    public void backByKey(KeyEvent keyEvent) {
+        if (isChoiceKey(keyEvent))
+            MainMenu.run();
+    }
+
+    public void changeRoundsByMouse(MouseEvent mouseEvent) {
+        if (isPrimary(mouseEvent))
+            changeRounds();
+    }
+
+    private void changeRounds() {
+        round = (round==1 ? 3 : 1);
+        message((round==1 ? "شمار دورهای بازی روی ۱ تنظیم شده است." : "شمار دورهای بازی روی ۳ تنظیم شده است." ), roundsGridPane);
+    }
+
+    public void changeRoundsByKey(KeyEvent keyEvent) {
+        if (isChoiceKey(keyEvent))
+            changeRounds();
+    }
+
+    public void playByMouse(MouseEvent mouseEvent) {
+        if (isPrimary(mouseEvent))
+            play();
+    }
+
+    private void play() {
+        String secondPlayer = getStringOfAGridPane(usernameOfOpponentGridPane);
+        boolean success = Controller.Menus.DuelMenu.duel(secondPlayer , ""+round);;
+        if (success) {
+            System.out.println("success");
+        } else {
+            message(Controller.Menus.DuelMenu.getError() , resultGridPane);
+        }
+    }
+
+    public void playByKey(KeyEvent keyEvent) {
+        if (isChoiceKey(keyEvent))
+                play();
     }
 }
