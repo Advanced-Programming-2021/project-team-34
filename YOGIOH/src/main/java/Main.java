1aa59cb2f7d4f69388bda646b17c7659e3d75af2
AliAgha888 <79265219+AliAgha888@users.noreply.github.com>
2021-07-13 01:56:42 +0430

    exception handling

--- YOGIOH/src/main/java/Main.java
+++ YOGIOH/src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             Model.User.loadAllUsers();
         } catch (IOException e) {
