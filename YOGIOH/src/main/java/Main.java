d5df9ed6f45140214f34557fa309681011a7aa13
AliAgha888 <79265219+AliAgha888@users.noreply.github.com>
2021-07-13 13:12:50 +0430

    signature removed

--- YOGIOH/src/main/java/Main.java
+++ YOGIOH/src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try {
             Model.User.loadAllUsers();
         } catch (IOException e) {
