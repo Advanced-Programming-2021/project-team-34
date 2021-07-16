58ead743e7dcc875d6680d6a7972f05a40532e62
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-07-16 21:27:50 +0430

    کلاس‌های مدل از کلاینت به سرور کپی شدند

--- server/src/main/java/Model/Monster.java
+++ server/src/main/java/Model/Monster.java
@@ -3,7 +3,6 @@ package Model;
 import Exceptions.NoMonsterWithThisNameException;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
