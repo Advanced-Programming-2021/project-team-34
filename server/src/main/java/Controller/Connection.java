7efa82131050a8a933030262eb872c3ce04a6fd9
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-07-16 17:16:42 +0430

    .نوشته‌شد Connection.java برای برقرای ارتباط با کلاینت در سرور کلاس

--- server/src/main/java/Controller/Connection.java
+++ server/src/main/java/Controller/Connection.java
@@ -1,4 +1,42 @@
 package Controller;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Connection {
+    final static int PORT = 12345;
+    static boolean toContinue = true;
+    public static void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            while (toContinue) {
+                Socket socket = serverSocket.accept();
+                new Thread(() -> {
+                    try {
+                        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                        String request = dataInputStream.readUTF();
+                        String result = RequestHandler.handleRequest(request);
+                        if (request.equals("end1342tth22234t53erfdweds3qwae123etrg2t3qwg321")) {
+                            toContinue = false;
+                        }
+                        dataOutputStream.writeUTF(result);
+
+                        dataInputStream.close();
+                        socket.close();
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
+                    }
+                }).start();
+            }
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
