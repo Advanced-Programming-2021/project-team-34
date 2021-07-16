83681bd1ebae97ded4439d23365e9bad3b4f90c2
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-07-16 21:22:42 +0430

    افزوده‌شدن Connection به کلاینت

---
+++ YOGIOH/src/main/java/Controller/Connection.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Connection {
+    static Socket socket;
+    static String host = "localhost";
+    public static String sendMessageToTheServer(String messageToSend){
+        try {
+            socket = new Socket(host , 12345);
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            dataOutputStream.writeUTF(messageToSend);
+            dataOutputStream.flush();
+            String result = dataInputStream.readUTF();
+
+            dataOutputStream.close();
+            socket.close();
+            return result;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "failed to connect to the server !";
+    }
+
+    public static void setHost(String host) {
+        Connection.host = host;
+    }
+}
