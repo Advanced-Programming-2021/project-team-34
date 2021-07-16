55705a85e78de213b3df8d3fb5bc494af330344f
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-07-16 17:14:36 +0430

    اقزوده‌شدن برخی رشته‌های ثابت برای مدیریت دستور ها در سرور

--- server/src/main/java/FinalStrings/CommandTypeMainParts.java
+++ server/src/main/java/FinalStrings/CommandTypeMainParts.java
@@ -1,2 +1,12 @@
-package FinalStrings;public class CommandTypeMainParts {
+package FinalStrings;
+
+public class CommandTypeMainParts {
+    public static final String LOGIN = "login";
+    public static final String SIGNUP = "signup";
+    public static final String LOGOUT = "logout";
+    public static final String CHANGE_NICKNAME = "change nickname";
+    public static final String CHANGE_PASSWORD = "change password";
+    public static final String CHANGE_AVATAR = "change avatar";
+    public static final String SEND_MESSAGE = "send message";
+    public static final String INCREASE_COIN = "increase coin";
 }
--- server/src/main/java/FinalStrings/CommandTypesFieldNames.java
+++ server/src/main/java/FinalStrings/CommandTypesFieldNames.java
@@ -4,4 +4,11 @@ public class CommandTypesFieldNames {
     public static final String USERNAME = "username";
     public static final String PASSWORD = "password";
     public static final String NICKNAME = "nickname";
+    public static final String TOKEN = "token";
+    public static final String NEW_NICKNAME = "newNickname";
+    public static final String NEW_PASSWORD = "newPassword";
+    public static final String CURRENT_PASSWORD = "currentPassword";
+    public static final String NEW_AVATAR = "newAvatar";
+    public static final String MESSAGE = "message";
+    public static final String VALUE = "value";
 }
