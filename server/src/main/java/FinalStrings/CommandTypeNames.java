05446002f11fc6561f2e8342512ab1630dc575fb
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-07-16 13:34:02 +0430

    افزوده‌شدن برخی رشته‌های ثابت

---
+++ server/src/main/java/FinalStrings/CommandTypeNames.java
@@ -0,0 +1,11 @@
+package FinalStrings;
+
+public class CommandTypeNames {
+    public static final String SIGH_UP = "sign up";
+    public static final String LOGIN = "login";
+    public static final String CHANGE_PASSWORD = "change password";
+    public static final String CHANGE_NICKNAME = "change nickname";
+    public static final String SEND_MESSAGE = "send message";
+    public static final String CHANGE_AVATAR = "change avatar";
+    public static final String NEW_GAME = "new game";
+}
---
+++ server/src/main/java/FinalStrings/CommandTypesFieldNames.java
@@ -0,0 +1,7 @@
+package FinalStrings;
+
+public class CommandTypesFieldNames {
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String NICKNAME = "nickname";
+}
