e8d989eb6b8795d8135c614f743a6540711f4919
AliAgha888 <79265219+AliAgha888@users.noreply.github.com>
2021-07-18 23:15:59 +0430

    changes for adding shop

--- server/src/main/java/FinalStrings/Results.java
+++ server/src/main/java/FinalStrings/Results.java
@@ -12,4 +12,9 @@ public class Results {
     public static final String USERNAME_AND_PASSWORD_DO_NOT_MATCH = "username and password don't match";
     public static final String USERNAME_IS_ALREADY_REGISTERED = "username is already registered";
     public static final String NICKNAME_IS_ALREADY_REGISTERED = "nickname is already registered";
+    public static final String NO_SUCH_CARD_EXISTS = "no such card exists";
+    public static final String FORBIDDEN_CARD = "forbidden card";
+    public static final String NOT_ENOUGH_MONEY = "not enough money";
+    public static final String THERE_IS_NO_CARD = "there is no card";
+    public static final String NO_ACCESS = "no access";
 }
