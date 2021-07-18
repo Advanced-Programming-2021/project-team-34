6a57a1b07f2d165dd9a14f10e82ed8861ecca213
MahdiTeymooriAnar <mahditeymoorianar@gmail.com>
2021-07-18 12:56:42 +0430

    برای یافتن دو کاربر با درخواست یک جور براز بازی GameRequestHandler.java در handle افزوده‌شدن

--- server/src/main/java/Controller/GameRequestsHandler.java
+++ server/src/main/java/Controller/GameRequestsHandler.java
@@ -1,5 +1,8 @@
 package Controller;
 
+import Model.GameRequest;
+import Model.User;
+
 public class GameRequestsHandler extends Thread {
     @Override
     public void run() {
@@ -8,5 +11,33 @@ public class GameRequestsHandler extends Thread {
 
     private void doMain() {
 
+    }
+
+    /**
+     * When this method be called , it checks if there is two users with same GameRequest and returns a two-home array of Users
+     *      and null if there are no two users with same GameRequest.
+     * @return User[] which is {user1 : User , user2 : User} if there are two users with same GameRequest
+     *      and is <b>null</b> if there are no two users with same request.
+     */
+    private static User[] handle() {
+        User aUserWhoHasRequestedOneRoundGame = null;
+        User aUserWhoHasRequestedThreeRoundGame = null;
+        for (User user :
+                User.users) {
+            if (user.getGameRequest().equals(GameRequest.ONE_ROUND)) {
+                if (aUserWhoHasRequestedOneRoundGame == null) {
+                    aUserWhoHasRequestedOneRoundGame = user;
+                } else {
+                    return new User[]{user, aUserWhoHasRequestedOneRoundGame};
+                }
+            } else if (user.getGameRequest().equals(GameRequest.THREE_ROUND)) {
+                if (aUserWhoHasRequestedThreeRoundGame == null) {
+                    aUserWhoHasRequestedThreeRoundGame = user;
+                } else {
+                    return new User[]{user, aUserWhoHasRequestedThreeRoundGame};
+                }
+            }
+        }
+        return null;
     }
 }
